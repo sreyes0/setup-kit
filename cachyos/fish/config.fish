4d5b46eceef337018e29f7aa923c7f9ddfa76789
sreyes0 <sreyes.haeusler@gmail.com>
2026-03-17 16:27:28 +0100

    First config.fish

---
+++ cachyos/fish/config.fish
@@ -0,0 +1,14 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+# Environment variables
+set -gx CONFIG ~/Documents/Projects/setup-kit
+
+# Aliases
+alias svim="sudo -E vim"
+alias dim="dimtoggle"
